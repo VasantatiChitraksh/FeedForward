39f78aa3f962ebafff13ed0cf7cd465421f67b90
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-08-07 22:53:43 +0530

    None

--- fbm_app/lib/Pages/leaderboard.dart
+++ fbm_app/lib/Pages/leaderboard.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+
+class leaderboard extends StatelessWidget {
+  const leaderboard({super.key});
 
   @override
   Widget build(BuildContext context) {
--- fbm_app/lib/main.dart
+++ fbm_app/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:fbm_app/Pages/FB_profile.dart';
 import 'package:fbm_app/Pages/Homepage.dart';
+import 'package:fbm_app/Pages/leaderboard.dart';
 import 'package:fbm_app/Pages/map.dart';
 import 'package:flutter/material.dart';
 import 'package:fbm_app/pages/leaderboard.dart';
@@ -25,7 +26,8 @@ class MyApp extends StatelessWidget {
       routes: {
         '/home': (context) => const Homepage(),
         '/profile': (context) => const FbProfile(),
-        '/map' : (context) => const Maap(),
+        '/map': (context) => const Maap(),
+        '/leaderboard': (context) => const leaderboard();
       },
     );
   }
