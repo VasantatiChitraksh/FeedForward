da5a661f4a70ce1a69ab3ceafc88523a182b6f4f
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-08-07 22:41:42 +0530

    pages

--- fbm_app/lib/Button/button.dart
+++ fbm_app/lib/Button/button.dart
@@ -1,21 +1,21 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-  final String text = "Hello";
+// class MyWidget extends StatelessWidget {
+//   const MyWidget({super.key});
+//   final String text = "Hello";
 
-  @override
-  Widget build(BuildContext context) { 
-    return Scaffold(
-      floatingActionButton: 
-      FloatingActionButton.extended(
-        label: Text(text,
-        style: TextStyle(fontWeight: FontWeight.w400,
-        color: Colors.black)), 
-        onPressed: () {
-          Navigator.pushNamed(context, routeName)
-          },) ,
-    );
-  }
+//   @override
+//   Widget build(BuildContext context) { 
+//     return Scaffold(
+//       floatingActionButton: 
+//       FloatingActionButton.extended(
+//         label: Text(text,
+//         style: TextStyle(fontWeight: FontWeight.w400,
+//         color: Colors.black)), 
+//         onPressed: () {
+//           Navigator.pushNamed(context, routeName)
+//           },) ,
+//     );
+//   }
 
-}
+// }
