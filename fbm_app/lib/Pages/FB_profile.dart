3e03e52b046536b8be7d2c3e131244abbd514583
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-08-08 15:04:55 +0530

    Pages

--- fbm_app/lib/Pages/FB_profile.dart
+++ fbm_app/lib/Pages/FB_profile.dart
@@ -1,34 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:fbm_app/Button/button.dart';
+
 class FbProfile extends StatelessWidget {
   const FbProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "Profile",style:TextStyle(fontWeight:FontWeight.bold ,color:Color.fromARGB(235, 0, 0, 0))
+        appBar: AppBar(
+          title: const Text("Profile",
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color.fromARGB(235, 0, 0, 0))),
+          centerTitle: true,
         ),
-        centerTitle: true;
-      ),
-      body:const Column(children: [
-        butt(
-          text:"My FB",
-          routeName:"/fb_info",
-          icon:Icon(Icons.food_bank)
-        ),
-         butt(
-          text:"My FB",
-          routeName:"/fb_info",
-          icon:Icon(Icons.food_bank)
-        ),
-          butt(
-          text:"My FB",
-          routeName:"/fb_info",
-          icon:Icon(Icons.food_bank)
-        )
-      ],)
-    );
+        body: const Column(
+          children: [
+            butt(
+                text: "My FB",
+                routeName: "/fb_info",
+                icon: Icon(Icons.food_bank)),
+            butt(text: "Donations", routeName: "", icon: Icon(Icons.favorite)),
+            butt(
+                text: "Volunteer_exp",
+                routeName: "",
+                icon: Icon(Icons.account_circle_outlined))
+          ],
+        ));
   }
 }
