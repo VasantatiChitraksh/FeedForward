1c3e1a420fe8472fa545c1401599c63241f20c6c
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-08-08 14:43:52 +0530

    Pages

--- fbm_app/lib/Pages/FB_profile.dart
+++ fbm_app/lib/Pages/FB_profile.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
-
+import 'package:fbm_app/Button/button.dart';
 class FbProfile extends StatelessWidget {
   const FbProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Profile",style:TextStyle(fontWeight:FontWeight.bold ,color:Color.fromARGB(235, 0, 0, 0))
+        ),
+        centerTitle: true;
+      ),
+      body:Column(children: [
+        butt(
+          text:"My FB",
+          routeName:"/fb_info",
+          icon:Icon(Icons.food_bank)
+        )
+         butt(
+          text:"My FB",
+          routeName:"/fb_info",
+          icon:Icon(Icons.food_bank)
+        )
+         butt(
+          text:"My FB",
+          routeName:"/fb_info",
+          icon:Icon(Icons.food_bank)
+        )
+
+
+
+
+
+
+
+
+
+      ],)
+    );
   }
 }
