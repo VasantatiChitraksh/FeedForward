4ac85ee0fe03c71fb2e373ac2364f71176bf6974
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-10 17:28:24 +0530

    added image pre processing in waste section

--- fbm_app/lib/Pages/wastemanagement.dart
+++ fbm_app/lib/Pages/wastemanagement.dart
@@ -1,7 +1,50 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'dart:io';
 
-class waste extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:http/http.dart' as http;
+
+class waste extends StatefulWidget {
   const waste({super.key});
+
+  @override
+  State<waste> createState() => _wasteState();
+}
+
+class _wasteState extends State<waste> {
+  XFile? imageSelected;
+  var output;
+
+  Future pickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (image != null) {
+      setState(() {
+        imageSelected = image;
+      });
+    }
+  }
+
+  Future sendImage() async {
+    final file = await File(imageSelected!.path);
+    final filebytes = await file.readAsBytes();
+    var request = http.MultipartRequest(
+        'POST', Uri.parse('http://127.0.0.1:5000/upload'));
+
+    request.files.add(http.MultipartFile.fromBytes(
+      'file',
+      filebytes,
+      filename: imageSelected!.name,
+    ));
+    var response = await request.send();
+    var responsebody = await response.stream.bytesToString();
+    var jsonOutput = jsonDecode(responsebody);
+    var Output = jsonOutput['output'];
+    print(Output);
+    setState(() {
+      output = Output;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +54,7 @@ class waste extends StatelessWidget {
         centerTitle: true,
       ),
       body: Column(
-        children: [
-          
-        ],
+        children: [],
       ),
     );
   }
