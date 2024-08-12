5f71bac11f3e1b39cc591756affbb519e89112d1
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-08-12 22:06:30 +0530

    fbm,volunteers

--- fbm_app/lib/Pages/Food_Bank_m.dart
+++ fbm_app/lib/Pages/Food_Bank_m.dart
@@ -20,70 +20,136 @@ class Food_Bank_Management extends StatelessWidget {
         ),
         centerTitle: true,
       ),
-      body: const Stack(
-        children: const [
-          Positioned(
-            top: 100,
-            left: 500,
-            child: Text(
-              "Food Bank Name:",
-              style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 24,
-                  color: Colors.black87,
-                  decoration: TextDecoration.underline),
+      body: Column(
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Food Bank Name:",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30,
+                    decoration: TextDecoration.underline),
+              ),
             ),
           ),
-          Positioned(
-            top: 175,
-            left: 500,
-            child: Text(
-              "Food Bank Adress:",
-              style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 24,
-                  color: Colors.black87,
-                  decoration: TextDecoration.underline),
+          SizedBox(
+            height: 5,
+          ),
+          TextField(
+            controller: TextEditingController(
+              text: 'Food Bank Name',
+            ),
+            readOnly: true, // Makes the field non-editable
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            style: TextStyle(
+              color: Colors.black,
             ),
           ),
-          Positioned(
-            top: 250,
-            left: 500,
-            child: Text(
-              "Contact Info:",
-              style: TextStyle(
-                  fontWeight: FontWeight.w300,
-                  fontSize: 24,
-                  color: Colors.black87,
-                  decoration: TextDecoration.underline),
+          SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Food Bank Adress:",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30,
+                    decoration: TextDecoration.underline),
+              ),
             ),
           ),
-          Positioned(
-            bottom: 175,
-            left: 620,
-            child: butt(
-              text: 'Inventory',
-              routeName: "/inventory",
-              icon: Icon(Icons.inventory),
+          SizedBox(height: 5),
+          TextField(
+            controller: TextEditingController(text: 'Food Bank Adress'),
+            readOnly: true, // Makes the field non-editable
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.white,
+            ),
+            style: TextStyle(
+              color: Colors.black,
             ),
           ),
-          Positioned(
-            bottom: 175,
-            left: 800,
-            child: butt(
-              text: 'Volunteers',
-              routeName: "/volunteers",
-              icon: Icon(Icons.volunteer_activism),
+          SizedBox(height: 30),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Contact Info:",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w400,
+                    fontSize: 30,
+                    decoration: TextDecoration.underline),
+              ),
             ),
           ),
-          Positioned(
-            bottom: 100,
-            left: 700,
-            child: butt(
-              text: 'Restaurants',
-              routeName: "/restaurants",
-              icon: Icon(Icons.restaurant_menu_rounded),
+          SizedBox(height: 5),
+          TextField(
+            controller: TextEditingController(text: 'Contact info'),
+            readOnly: true, // Makes the field non-editable
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              filled: true,
+              fillColor: Colors.white,
             ),
+            style: TextStyle(
+              color: Colors.black,
+            ),
+          ),
+          SizedBox(
+            height: 2,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Row(
+            children: [
+              SizedBox(
+                width: 375,
+              ),
+              butt(
+                text: "Inventory",
+                routeName: "/inventory",
+                icon: Icon(Icons.food_bank),
+              ),
+              SizedBox(
+                width: 500,
+              ),
+              butt(
+                text: "volunteers",
+                routeName: "/volunteers",
+                icon: Icon(Icons.favorite),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          butt(
+            text: "Restarunts",
+            routeName: "/restarunts",
+            icon: Icon(Icons.account_circle_outlined),
           ),
         ],
       ),
