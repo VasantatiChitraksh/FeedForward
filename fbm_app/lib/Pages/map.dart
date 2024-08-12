6b6f6933e9c31d1fe900c926095c4300787a1a0a
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-13 04:35:00 +0530

    updated map.dart
    connect map.dart to databse to show markers for location of foodbanks fetched from databases

--- fbm_app/lib/Pages/map.dart
+++ fbm_app/lib/Pages/map.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:geolocator/geolocator.dart';
@@ -12,13 +13,35 @@ class MapOutlets extends StatefulWidget {
 }
 
 class _MapOutletsState extends State<MapOutlets> {
+  List<Map<String, LatLng>> outlets = [];
+
   LatLng _center = LatLng(0, 0);
   MapController _mapController = MapController();
   List<Marker> markers = [];
 
+  @override
   void initState() {
     super.initState();
     _requestPermission();
+    loadFoodBankDetails();
+  }
+
+  Future<void> loadFoodBankDetails() async {
+    CollectionReference foodbankcollection =
+        FirebaseFirestore.instance.collection('foodbank');
+
+    QuerySnapshot queryfoodbank = await foodbankcollection.get();
+
+    for (var doc in queryfoodbank.docs) {
+      String name = doc['name'];
+      var loc = doc['location'];
+      LatLng latlng = LatLng(loc['latitude'], loc['longitude']);
+      outlets.add({name: latlng});
+      markers.add(Marker(
+        point: latlng,
+        builder: (context) => Icon(Icons.location_pin, color: Colors.red),
+      ));
+    }
   }
 
   Future<void> _requestPermission() async {
@@ -51,9 +74,10 @@ class _MapOutletsState extends State<MapOutlets> {
       body: Padding(
         padding: const EdgeInsets.all(15.0),
         child: Card(
-          child: MapWidget(center: _center, mapController: _mapController, markers: markers),
+          child: MapWidget(
+              center: _center, mapController: _mapController, markers: markers),
           elevation: 4,
-          ),
+        ),
       ),
     );
   }
@@ -65,7 +89,8 @@ class MapWidget extends StatelessWidget {
     required LatLng center,
     required MapController mapController,
     required this.markers,
-  }) : _center = center, _mapController = mapController;
+  })  : _center = center,
+        _mapController = mapController;
 
   final LatLng _center;
   final MapController _mapController;
