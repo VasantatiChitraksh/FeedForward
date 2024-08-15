f395e3e0f06b9de877643b18ddb3f0a90a90acdd
CharanTej-3457 <cs23b011@iittp.ac.in>
2024-08-15 14:32:03 +0530

    ......

--- fbm_app/lib/Pages/Volunteers.dart
+++ fbm_app/lib/Pages/Volunteers.dart
@@ -1,4 +1,4 @@
-import 'package:fbm_app/Styles/BgColor.dart';
+import 'package:fbm_app/Button/button.dart';
 import 'package:flutter/material.dart';
 
 class Volunteers extends StatelessWidget {
@@ -8,53 +8,64 @@ class Volunteers extends StatelessWidget {
   Widget build(BuildContext context) {
     final List<String> volunteer = ['V1', 'V2', 'V3', 'v4', 'V5', 'V6', 'V7'];
     return Scaffold(
-      backgroundColor: AppTheme.primaryColor,
-      appBar: AppBar(
-        backgroundColor: AppTheme.secondaryColor,
-        title: Text(
-          "Volunteers",
-          style: TextStyle(
-              fontWeight: FontWeight.bold, color: Color.fromARGB(255, 0, 0, 0)),
+        appBar: AppBar(
+          title: Text(
+            "Volunteers",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 0, 0, 0)),
+          ),
         ),
-      ),
-      body: ListView.builder(
-        itemCount: volunteer.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Text(
-              volunteer[index], // Start numbering from 1
-              style: TextStyle(
-                fontSize: 30,
+        body: Stack(
+          children: [
+            ListView.builder(
+              itemCount: volunteer.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  leading: Text(
+                    volunteer[index], // Start numbering from 1
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  ),
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextField(
+                        controller:
+                            TextEditingController(text: 'Volunteer Name'),
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      TextField(
+                        controller: TextEditingController(text: 'Contact Info'),
+                        readOnly: true,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          fillColor: Colors.white,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
+                  ),
+                );
+              },
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child:
+                    butt(icon: Icon(Icons.add), routeName: "/vform", text: ""),
               ),
             ),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: TextEditingController(text: 'Volunteer Name'),
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 10),
-                TextField(
-                  controller: TextEditingController(text: 'Contact Info'),
-                  readOnly: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    filled: true,
-                    fillColor: Colors.white,
-                  ),
-                ),
-                SizedBox(height: 10),
-              ],
-            ),
-          );
-        },
-      ),
-    );
+          ],
+        ));
   }
 }
