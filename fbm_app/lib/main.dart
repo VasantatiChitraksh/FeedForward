e1e8c147dff4c824207929ac4d76f581cd531c2b
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-15 20:21:54 +0530

    Update main.dart

--- fbm_app/lib/main.dart
+++ fbm_app/lib/main.dart
@@ -10,6 +10,7 @@ import 'package:fbm_app/Pages/Restaurant/fbanks_connected.dart';
 import 'package:fbm_app/Pages/Restaurant/listRestaurants.dart';
 import 'package:fbm_app/Pages/Volunteers.dart';
 import 'package:fbm_app/Pages/aunthication/login_screen.dart';
+import 'package:fbm_app/Pages/emergencypage.dart';
 import 'package:fbm_app/Pages/leaderboard.dart';
 import 'package:fbm_app/Pages/map.dart';
 import 'package:fbm_app/Pages/volunteer_reg.dart';
