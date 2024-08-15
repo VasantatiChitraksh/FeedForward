8a33611f1af6bb0f208f914a7d053b52440bc4be
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-15 20:18:53 +0530

    Update modelCNN.py

--- PythonServer/modelCNN.py
+++ PythonServer/modelCNN.py
@@ -10,49 +10,6 @@ import zipfile
 
 app = Flask(__name__)
 
-
-# def download_model_from_drive(model_id, model_path):
-#     def get_confirm_token(response):
-#         for key, value in response.cookies.items():
-#             if key.startswith('download_warning'):
-#                 return value
-#         return None
-
-#     def save_response_content(response, model_path):
-#         CHUNK_SIZE = 32768
-
-#         with open(model_path, "wb") as file:
-#             for chunk in response.iter_content(CHUNK_SIZE):
-#                 if chunk:  # Filter out keep-alive new chunks
-#                     file.write(chunk)
-
-#     URL = "https://docs.google.com/uc?export=download"
-#     session = requests.Session()
-
-#     response = session.get(URL, params={'id': model_id}, stream=True)
-#     token = get_confirm_token(response)
-
-#     if token:
-#         params = {'id': model_id, 'confirm': token}
-#         response = session.get(URL, params=params, stream=True)
-
-#     save_response_content(response, model_path)
-#     print(f"Model downloaded to {model_path}")
-
-
-
-
-# # https://drive.google.com/file/d/1-CyZBUzBd2FZIvawGrRU_7xvdwmSH_GX/view?usp=sharing
-
-
-# MODEL_FILE_ID = "1-CyZBUzBd2FZIvawGrRU_7xvdwmSH_GX"
-# MODEL_PATH = 'WasteClassifierModel.keras'
-
-# download_model_from_drive(MODEL_FILE_ID, MODEL_PATH)
-
-# with zipfile.ZipFile(os.path.join(MODEL_PATH, "model.zip"), 'r') as zip_ref:
-#     zip_ref.extractall(MODEL_PATH)
-
 model = tf.keras.models.load_model('WasteDetectionModel(1).h5')
 
 
