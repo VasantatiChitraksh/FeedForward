afcec4e5b6d3c2e6a067ad2bc793559ff673e2cb
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-18 23:31:56 +0530

    Revert "--"
    This reverts commit 8ed84169940ac85d07d669d6ad8f02c3a7de4049.

--- fbm_app/lib/Pages/Forms/donationform.dart
+++ fbm_app/lib/Pages/Forms/donationform.dart
@@ -11,6 +11,10 @@ class Donation_form extends StatefulWidget {
 }
 
 class _DonationFormState extends State<Donation_form> {
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
   final List<String> _selected = [
     'Rice(kg/g)',
     'pulses(g)',
@@ -19,6 +23,19 @@ class _DonationFormState extends State<Donation_form> {
     'Bread'
   ];
   String Selected_item = "Rice";
+=======
+=======
+>>>>>>> parent of ee889fe (Revert)
+=======
+>>>>>>> parent of ee889fe (Revert)
+=======
+>>>>>>> parent of ee889fe (Revert)
+  static List<ItemDonationWidget> widgets = [];
+
+  void addWidgets() {
+    widgets.add(const ItemDonationWidget());
+  }
+>>>>>>> parent of ee889fe (Revert)
 
   @override
   Widget build(BuildContext context) {
@@ -35,6 +52,10 @@ class _DonationFormState extends State<Donation_form> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
               SizedBox(height: 10),
               SizedBox(height: 10),
               Padding(
@@ -98,8 +119,23 @@ class _DonationFormState extends State<Donation_form> {
                         hintStyle: TextStyle(color: Colors.black54),
                         border: OutlineInputBorder())),
               ),
+              Row(children: [
+=======
+              for(int i=0; i<widgets.length; i++) widgets[i],
               const Row(children: [
-               Row(children: [
+>>>>>>> parent of ee889fe (Revert)
+=======
+              for(int i=0; i<widgets.length; i++) widgets[i],
+              const Row(children: [
+>>>>>>> parent of ee889fe (Revert)
+=======
+              for(int i=0; i<widgets.length; i++) widgets[i],
+              const Row(children: [
+>>>>>>> parent of ee889fe (Revert)
+=======
+              for(int i=0; i<widgets.length; i++) widgets[i],
+              const Row(children: [
+>>>>>>> parent of ee889fe (Revert)
                 SizedBox(height: 40, width: 175),
                 Padding(
                   padding: const EdgeInsets.all(8.0),
@@ -108,11 +144,83 @@ class _DonationFormState extends State<Donation_form> {
                       icon: Icon(Icons.handshake_rounded),
                       routeName: ''),
                 ),
+<<<<<<< HEAD
               ])
+=======
               ]),
+              FloatingActionButton(onPressed: addWidgets)
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+>>>>>>> parent of ee889fe (Revert)
+=======
+>>>>>>> parent of ee889fe (Revert)
+=======
+>>>>>>> parent of ee889fe (Revert)
+=======
+>>>>>>> parent of ee889fe (Revert)
             ],
           ),
         ));
   }
 }
+<<<<<<< HEAD
+=======
 
+class ItemDonationWidget extends StatefulWidget {
+  const ItemDonationWidget({super.key});
+
+  @override
+  State<ItemDonationWidget> createState() => _ItemDonationWidgetState();
+}
+
+class _ItemDonationWidgetState extends State<ItemDonationWidget> {
+  final List<String> _selected = [
+    'Rice(kg/g)',
+    'pulses(g)',
+    'Complex_meals',
+    'Simple_meals',
+    'Bread'
+  ];
+  String Selected_item = "Rice";
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+                padding: const EdgeInsets.all(8.0),
+                child:
+                    Text_Theme.text_colored("Food Type-1", 24, Colors.black87),
+              ),
+              SizedBox(height: 10),
+              DropdownButton<String>(
+                value: Selected_item,
+                items: _selected.map(
+                  (String dropDownStringItem) {
+                    return DropdownMenuItem(
+                      value: dropDownStringItem,
+                      child: Text(dropDownStringItem),
+                    );
+                  },
+                ).toList(),
+                onChanged: (String? newSelectedValue) {
+                  setState(() {
+                    Selected_item = newSelectedValue!;
+                  });
+                },
+              ),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: TextField(
+                    decoration: InputDecoration(
+                        hintText: "Enter your quantity",
+                        hintStyle: TextStyle(color: Colors.black54),
+                        border: OutlineInputBorder())),
+              ),
+        ],
+      ),
+    );
+  }
+}
+>>>>>>> parent of ee889fe (Revert)
