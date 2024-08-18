228f6621b2c2b90a8df0ccb923514832e952c0f9
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-18 16:24:31 +0530

    Revert "yo"
    This reverts commit e47c9b65f12792ff25375e04f5f747ae1c43a787.

--- fbm_app/lib/Pages/Forms/donationform.dart
+++ fbm_app/lib/Pages/Forms/donationform.dart
@@ -4,21 +4,19 @@ import 'package:flutter/widgets.dart';
 import 'package:fbm_app/Styles/TextStyle.dart';
 import 'package:fbm_app/Button/button.dart';
 
-class Donation_form extends StatefulWidget {
-  const Donation_form({super.key});
+class DonationForm extends StatefulWidget {
+  const DonationForm({super.key});
+
   @override
-  _DonationFormState createState() => _DonationFormState();
+  State<DonationForm> createState() => _DonationFormState();
 }
 
-class _DonationFormState extends State<Donation_form> {
-  final List<String> _selected = [
-    'Rice',
-    'wheat',
-    'Pulses',
-    'Red Gram',
-    'Ragi'
-  ];
-  String Selected_item = "Rice";
+class _DonationFormState extends State<DonationForm> {
+  static List<ItemDonationWidget> widgets = [];
+
+  void addWidgets() {
+    widgets.add(const ItemDonationWidget());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,81 +32,82 @@ class _DonationFormState extends State<Donation_form> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 10),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:
-                    Text_Theme.text_colored("Food Type-1", 24, Colors.black87),
-              ),
-              SizedBox(height: 10),
-              DropdownButton<String>(
-                value: Selected_item,
-                items: _selected.map(
-                  (String dropDownStringItem) {
-                    return DropdownMenuItem(
-                      value: dropDownStringItem,
-                      child: Text(dropDownStringItem),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? newSelectedValue) {
-                  setState(() {
-                    this.Selected_item = newSelectedValue!;
-                  });
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Enter your quantity",
-                        hintStyle: TextStyle(color: Colors.black54),
-                        border: OutlineInputBorder())),
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:
-                    Text_Theme.text_colored("Food Type-2", 24, Colors.black87),
-              ),
-              DropdownButton<String>(
-                value: Selected_item,
-                items: _selected.map(
-                  (String dropDownStringItem) {
-                    return DropdownMenuItem(
-                      value: dropDownStringItem,
-                      child: Text(dropDownStringItem),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? newSelectedValue) {
-                  setState(() {
-                    this.Selected_item = newSelectedValue!;
-                  });
-                },
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Enter your quantity",
-                        hintStyle: TextStyle(color: Colors.black54),
-                        border: OutlineInputBorder())),
-              ),
-              Row(children: [
+              const SizedBox(height: 20),
+              //
+              for (int i = 0; i < widgets.length; i++) widgets[i],
+              //
+              const Row(children: [
                 SizedBox(height: 40, width: 175),
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
+                  padding: EdgeInsets.all(8.0),
                   child: butt(
                       text: 'Donate',
                       icon: Icon(Icons.handshake_rounded),
                       routeName: ''),
                 ),
-              ])
+              ]),
+              FloatingActionButton(onPressed: addWidgets)
             ],
           ),
         ));
+  }
+}
+
+class ItemDonationWidget extends StatefulWidget {
+  const ItemDonationWidget({super.key});
+
+  @override
+  State<ItemDonationWidget> createState() => _ItemDonationWidgetState();
+}
+
+class _ItemDonationWidgetState extends State<ItemDonationWidget> {
+  final List<String> _selected = [
+    'Rice',
+    'Bread',
+    'Pulses',
+    'SimpleMeals',
+    'ComplexMeals',
+  ];
+  String Selected_item = "Rice";
+  TextEditingController controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child:
+                Text_Theme.text_colored("Select Food Type", 24, Colors.black87),
+          ),
+          const SizedBox(height: 10),
+          DropdownButton<String>(
+            value: Selected_item,
+            items: _selected.map(
+              (String dropDownStringItem) {
+                return DropdownMenuItem(
+                  value: dropDownStringItem,
+                  child: Text(dropDownStringItem),
+                );
+              },
+            ).toList(),
+            onChanged: (String? newSelectedValue) {
+              setState(() {
+                Selected_item = newSelectedValue!;
+              });
+            },
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+                controller: controller,
+                decoration: const InputDecoration(
+                    hintText: "Enter your quantity in kg/meals",
+                    hintStyle: TextStyle(color: Colors.black54),
+                    border: OutlineInputBorder())),
+          ),
+        ],
+      ),
+    );
   }
 }
