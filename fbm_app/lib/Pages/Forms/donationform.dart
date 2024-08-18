434cdcad6abfc3925e618142c7bd2bc9b0b2f7a7
Anirudh Arrepu <anirudharrepu@gmail.com>
2024-08-18 22:36:25 +0530

    reverted reverted changes

--- fbm_app/lib/Pages/Forms/donationform.dart
+++ fbm_app/lib/Pages/Forms/donationform.dart
@@ -11,14 +11,11 @@ class Donation_form extends StatefulWidget {
 }
 
 class _DonationFormState extends State<Donation_form> {
-  final List<String> _selected = [
-    'Rice(kg/g)',
-    'pulses(g)',
-    'Complex_meals',
-    'Simple_meals',
-    'Bread'
-  ];
-  String Selected_item = "Rice";
+  static List<ItemDonationWidget> widgets = [];
+
+  void addWidgets() {
+    widgets.add(const ItemDonationWidget());
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -35,9 +32,45 @@ class _DonationFormState extends State<Donation_form> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-              SizedBox(height: 10),
-              SizedBox(height: 10),
-              Padding(
+              const Row(children: [
+                SizedBox(height: 40, width: 175),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: butt(
+                      text: 'Donate',
+                      icon: Icon(Icons.handshake_rounded),
+                      routeName: ''),
+                ),
+              ]),
+              FloatingActionButton(onPressed: addWidgets)
+            ],
+          ),
+        ));
+  }
+}
+
+class ItemDonationWidget extends StatefulWidget {
+  const ItemDonationWidget({super.key});
+
+  @override
+  State<ItemDonationWidget> createState() => _ItemDonationWidgetState();
+}
+
+class _ItemDonationWidgetState extends State<ItemDonationWidget> {
+  final List<String> _selected = [
+    'Rice(kg/g)',
+    'pulses(g)',
+    'Complex_meals',
+    'Simple_meals',
+    'Bread'
+  ];
+  String Selected_item = "Rice";
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: [
+          Padding(
                 padding: const EdgeInsets.all(8.0),
                 child:
                     Text_Theme.text_colored("Food Type-1", 24, Colors.black87),
@@ -55,61 +88,20 @@ class _DonationFormState extends State<Donation_form> {
                 ).toList(),
                 onChanged: (String? newSelectedValue) {
                   setState(() {
-                    this.Selected_item = newSelectedValue!;
+                    Selected_item = newSelectedValue!;
                   });
                 },
               ),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
                 child: TextField(
                     decoration: InputDecoration(
                         hintText: "Enter your quantity",
                         hintStyle: TextStyle(color: Colors.black54),
                         border: OutlineInputBorder())),
               ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child:
-                    Text_Theme.text_colored("Food Type-2", 24, Colors.black87),
-              ),
-              DropdownButton<String>(
-                value: Selected_item,
-                items: _selected.map(
-                  (String dropDownStringItem) {
-                    return DropdownMenuItem(
-                      value: dropDownStringItem,
-                      child: Text(dropDownStringItem),
-                    );
-                  },
-                ).toList(),
-                onChanged: (String? newSelectedValue) {
-                  setState(() {
-                    this.Selected_item = newSelectedValue!;
-                  });
-                },
-              ),
-              SizedBox(height: 10),
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: TextField(
-                    decoration: InputDecoration(
-                        hintText: "Enter your quantity",
-                        hintStyle: TextStyle(color: Colors.black54),
-                        border: OutlineInputBorder())),
-              ),
-              Row(children: [
-                SizedBox(height: 40, width: 175),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: butt(
-                      text: 'Donate',
-                      icon: Icon(Icons.handshake_rounded),
-                      routeName: ''),
-                ),
-              ])
-            ],
-          ),
-        ));
+        ],
+      ),
+    );
   }
 }
