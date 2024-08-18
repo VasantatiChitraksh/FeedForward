f53c0d1349c82c2874dcb719c683d867da8580e3
Chitraksh Vasantati <149754440+VasantatiChitraksh@users.noreply.github.com>
2024-08-18 19:33:23 +0530

    yo

--- fbm_app/lib/Pages/Forms/volunteer_reg.dart
+++ fbm_app/lib/Pages/Forms/volunteer_reg.dart
@@ -1,8 +1,7 @@
 import 'package:fbm_app/Styles/BgColor.dart';
 import 'package:fbm_app/Styles/TextStyle.dart';
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart'; 
-
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class VolunteerForm extends StatefulWidget {
   @override
@@ -10,20 +9,18 @@ class VolunteerForm extends StatefulWidget {
 }
 
 class _VolunteerFormState extends State<VolunteerForm> {
-  final TextEditingController _userNameController =
-      TextEditingController();
-  final TextEditingController _hoursController =
-      TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  final TextEditingController _hoursController = TextEditingController();
+  final TextEditingController _foodbankController = TextEditingController();
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
- 
-  Future<void> _saveUserData(String userName, int hoursWorked, String foodbank) async {
+  Future<void> _saveUserData(
+      String userName, int hoursWorked, String foodbank) async {
     try {
-    
       await _firestore.collection('volunteers').add({
         'userName': userName,
         'hoursWorked': hoursWorked,
-        'foodbank':foodbank,
+        'foodbank': foodbank,
       });
       print("Data saved successfully");
     } catch (e) {
@@ -34,35 +31,38 @@ class _VolunteerFormState extends State<VolunteerForm> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppTheme.bgcolor(),
-      appBar: AppBar(
-        elevation: 10,
-        backgroundColor: AppTheme.bgcolor(),
-        title: const Text(
-          'Volunteer Form',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 32,
-            color: Color.fromARGB(255, 248, 248, 248),
+        backgroundColor: AppTheme.primaryColor,
+        appBar: AppBar(
+          elevation: 10,
+          backgroundColor: Colors.white,
+          title: const Text(
+            'Volunteer Form',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 32,
+              color: Color.fromARGB(255, 0, 0, 0),
+            ),
           ),
+          centerTitle: false,
         ),
-        centerTitle: true,
-      ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: <Widget>[
-              SizedBox(height: 200),
-              TextField(
-                controller: _userNameController,
-                decoration: InputDecoration(
-                  labelText: 'Enter the username',
-                  labelStyle: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 24,
-                      color: Colors.white),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(height: 50),
+                TextField(
+                  controller: _userNameController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter the username',
+                    labelStyle: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                        color: Colors.white),
+                  ),
                 ),
+<<<<<<< Updated upstream
               ),
             
            // SizedBox(height: 200),
@@ -100,5 +100,56 @@ class _VolunteerFormState extends State<VolunteerForm> {
         ),
       ),
     ));
+=======
+                SizedBox(height: 25),
+                TextField(
+                  controller: _hoursController,
+                  decoration: InputDecoration(
+                    labelText: 'Number of hours worked',
+                    labelStyle: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 25),
+                TextField(
+                  controller: _foodbankController,
+                  decoration: InputDecoration(
+                    labelText: 'Enter the FoodBank:',
+                    labelStyle: TextStyle(
+                        fontWeight: FontWeight.w700,
+                        fontSize: 24,
+                        color: Colors.white),
+                  ),
+                ),
+                SizedBox(height: 50),
+                ElevatedButton(
+                    onPressed: () async {
+                      // Parse the hours worked input to an integer
+                      int hoursWorked =
+                          int.tryParse(_hoursController.text) ?? 0;
+
+                      // Call the function to save the data to Firestore
+                      await _saveUserData(_userNameController.text, hoursWorked,
+                          _foodbankController.text);
+
+                      // Show a Snackbar to confirm data submission
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(content: Text('Data saved successfully')),
+                      );
+
+                      // Clear the input fields after submission
+                      _userNameController.clear();
+                      _hoursController.clear();
+                    },
+                    child: Text(
+                      "SUBMIT",
+                    ))
+              ],
+            ),
+          ),
+        ));
+>>>>>>> Stashed changes
   }
 }
